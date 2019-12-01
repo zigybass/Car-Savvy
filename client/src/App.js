d2d5c268966e12bda5111df64435032369629880
Zigy <zigybass@gmail.com>
2019-12-01 17:27:58 -0500

    Built Header componenent. Spent most time on Adobe XD

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from "../src/components/Header";
 
 class App extends React.Component {
 
   render () {
   return (
     <div className="App">
+      <Header />
       <div className="container">
         <h4>CarSavvy</h4>
       </div>
---
+++ client/src/components/Header.js
@@ -0,0 +1,19 @@
+const React = require("react");
+
+class Header extends React.Component {
+
+    render () {
+
+        return (
+
+        <div className="header">
+            <div>
+                <p>This is a header</p> 
+            </div>
+        </div>
+        )
+    }
+
+}
+
+export default Header;
