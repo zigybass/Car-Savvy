96ce5bd6a51a070922b02ded87cafdedf067ce77
Zigy <zigybass@gmail.com>
2019-11-30 21:46:34 -0500

    init React client folder

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class App extends React.Component {
+
+  render () {
+  return (
+    <div className="App">
+      <div className="container">
+        <h4>CarSavvy</h4>
+      </div>
+    </div>
+  );
+  }
+}
+
+export default App;
