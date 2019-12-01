595d3744a421a2f146a31156605d9146b162f08d
Zigy <zigybass@gmail.com>
2019-11-30 21:38:25 -0500

    some backend files + server

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log("Server listening on PORT: " + PORT)
+})
