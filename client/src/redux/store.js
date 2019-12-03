c3867696860ad33bbd7dd569649f53be1a1db451
Zigy <zigybass@gmail.com>
2019-12-03 17:23:07 -0500

    createStore added. Needs reducers, correct state

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,7 +1,21 @@
-import React from "react";
+import { createStore } from 'redux';
 
-const store = {
-    test: "test"
-};
+function todos(state = [], action) {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return state.concat([action.text])
+    default:
+      return state
+  }
+}
+
+const store = createStore(todos, ['Use Redux'])
+
+store.dispatch({
+  type: 'ADD_TODO',
+  text: 'Read the docs'
+})
+
+console.log(store.getState())
 
 export default store;
